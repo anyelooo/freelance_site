a05eb418b940a1f9700321f7aa68c2ec8412cf3c
anyelooo <cruzhurtadoj@gmail.com>
2021-10-05 15:12:14 -0500

    Primer commit

---
+++ sw.js
@@ -0,0 +1,58 @@
+//cramos y asignamos nombre y valor al cache de la App
+const CACHE_NAME='V1_portafolio',
+urlsToCache=[
+    './',
+    'http://www.w3.org/2000/svg',
+    './static/css/normalize.css',
+    './static/css/principal.css',
+    './static/img/p1.jpg',
+]
+
+//Funcion que ayudara con la instalacion de la apliacion
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache)
+                .then(() => self.skipWaiting())
+        })
+        .catch(err => console.log('Falló el registro de cache', err))
+    )
+})
+
+//Funcion que nos ayuda a que una vez instalado el SW la App funcione sin conexion
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME]
+
+    e.waitUntil(
+        caches.keys()
+        .then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    //Eliminamos lo que ya no se necesita en cache
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
+        })
+        // Le indica al SW activar el cache actual
+        .then(() => self.clients.claim())
+    )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+    //Responder ya sea con el objeto en caché o continuar y buscar la url real
+    e.respondWith(
+        caches.match(e.request)
+        .then(res => {
+            if (res) {
+                //recuperar del cache
+                return res
+            }
+            //recuperar de la petición a la url
+            return fetch(e.request)
+        })
+    )
+})
